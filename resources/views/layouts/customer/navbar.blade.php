1c8bc86cc21d0d4475de8f4b8bb3fa62f0936264
Mykhailo <mykhailotomov@gmail.com>
2019-05-21 19:32:45 +0300

    navbar 50%

--- resources/views/layouts/customer/navbar.blade.php
+++ resources/views/layouts/customer/navbar.blade.php
@@ -1,24 +1,55 @@
-<nav class="navbar navbar-expand-lg bg-primary">
-  <div class="container">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
+<nav class="navbar navbar-expand-lg fixed-top" color-on-scroll="300">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="#" rel="tooltip" title="{{env('APP_NAME')}}" data-placement="bottom" target="_blank">
+          givu logo
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+            <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">News</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Partners</a>
+            </li>
+        </ul>
+        <div class="justify-content-end">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/" target="_blank">
+                <i class="fa fa-twitter"></i>
+                <p class="d-lg-none">Twitter</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/" target="_blank">
+                <i class="fa fa-facebook-square"></i>
+                <p class="d-lg-none">Facebook</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/" target="_blank">
+                <i class="fa fa-instagram"></i>
+                <p class="d-lg-none">Instagram</p>
+                </a>
+            </li>            
+            <li class="nav-item">
+                <a href="#" target="_blank" class="btn btn-light btn-round">Log-in</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" target="_blank" class="btn btn-danger btn-round">Sign-up</a>
+            </li>
+            </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
