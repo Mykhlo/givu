688999319881a2de94c085475d96171ad086145e
Mykhailo <mykhailotomov@gmail.com>
2019-06-03 21:40:17 +0300

    organizations categories model/migration/seed/controller created

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -20,9 +20,9 @@
         </div>        
     </div>
 </div>
-    @include('_partials.navbar2')
- <div class="mt-4">
-    <div class="row">
+    <!-- navdar 2 temporary disabled -->
+ <div class="row mt-4">
+    
           <div class="col-lg-5">
             <div class="card" data-background="image" style="background-image: url('https://www.goodeed.com/pictures/project/0b1849e7-721a-463f-ab85-8f3d52ea82c4.JPEG')">
                 <div class="btn-group-toggle favourite" data-toggle="buttons">
@@ -30,18 +30,19 @@
                     <input type="checkbox" alt="Submit" checked autocomplete="off"> <i class="nc-icon nc-favourite-28"></i>
                   </label>
                 </div>            
-              <div class="card-body">
-                <h6 class="card-category">Header 1</h6>
+              <div class="card-body pt-4">
+                <!-- <h6 class="card-category">Header 1</h6> -->
                 <a href="#">
-                  <h3 class="card-title">Header 2</h3>
+                  <h3 class="card-title mt-0">Environment</h3>
                 </a>
                 <p class="card-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
                 </p>
-                <div class="btn-group" role="group" aria-label="Basic example">
+                <div class="btn-group col-8 px-0" role="group" aria-label="Basic example">
                     <button type="button" class="btn btn-danger btn-round">Donate</button>
                     <button type="button" class="btn btn-success btn-round">Donate for free</button>                    
                 </div>
+                <button type="button" class=" col-8 mt-2 btn btn-outline-danger btn-round">quick free donation</button>
               </div>
             </div>
           </div>
@@ -52,23 +53,23 @@
                     <input type="checkbox" alt="Submit" autocomplete="off"> <i class="nc-icon nc-favourite-28"></i>
                   </label>
                 </div>
-                <div class="card-body">
-                <h6 class="card-category">Header 1</h6>
+                <div class="card-body pt-4">
+                <!-- <h6 class="card-category">Header 1</h6> -->
                 <a href="#">
-                    <h3 class="card-title">Header 2</h3>
+                    <h3 class="card-title mt-0">Animal rights</h3>
                 </a>
                 <p class="card-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
                 </p>
-                <div class="btn-group" role="group" aria-label="Basic example">
+                <div class="btn-group col-8 px-0" role="group" aria-label="Basic example">
                     <button type="button" class="btn btn-danger btn-round">Donate</button>
                     <button type="button" class="btn btn-success btn-round">Donate for free</button>                    
                 </div>
+                <button type="button" class=" col-8 mt-2 btn btn-outline-danger btn-round">quick free donation</button>
               </div>
             </div>
           </div>
-        </div>
-        <div class="row">
+
           <div class="col-lg-7">
             <div class="card" data-background="image" style="background-image: url('https://www.goodeed.com/pictures/project/524e6846-5776-4916-9525-dee2d46e385c.JPEG')">
               <div class="btn-group-toggle favourite" data-toggle="buttons">
@@ -77,9 +78,9 @@
                   </label>
               </div>
               <div class="card-body">
-                    <h6 class="card-category">Header 1</h6>
+                    <!-- <h6 class="card-category">Header 1</h6> -->
                 <a href="#">
-                    <h3 class="card-title">Header 2</h3>
+                    <h3 class="card-title">Human rights</h3>
                 </a>
                 <p class="card-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
@@ -99,9 +100,9 @@
                   </label>
               </div>
               <div class="card-body">
-                    <h6 class="card-category">Header 1</h6>
+                    <!-- <h6 class="card-category">Header 1</h6> -->
                 <a href="#">
-                    <h3 class="card-title">Header 2</h3>
+                    <h3 class="card-title">Education</h3>
                 </a>
                 <p class="card-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
@@ -113,8 +114,7 @@
               </div>
             </div>
           </div>
-        </div>
-        <div class="row">
+
           <div class="col-lg-5">
             <div class="card" data-background="image" style="background-image: url('https://www.goodeed.com/pictures/project/0b1849e7-721a-463f-ab85-8f3d52ea82c4.JPEG')">
                 <div class="btn-group-toggle favourite" data-toggle="buttons">
@@ -123,9 +123,9 @@
                   </label>
                 </div>            
               <div class="card-body">
-                <h6 class="card-category">Header 1</h6>
+                <!-- <h6 class="card-category">Header 1</h6> -->
                 <a href="#">
-                  <h3 class="card-title">Header 2</h3>
+                  <h3 class="card-title">Development</h3>
                 </a>
                 <p class="card-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
@@ -145,9 +145,9 @@
                   </label>
                 </div>
                 <div class="card-body">
-                <h6 class="card-category">Header 1</h6>
+                <!-- <h6 class="card-category">Header 1</h6> -->
                 <a href="#">
-                    <h3 class="card-title">Header 2</h3>
+                    <h3 class="card-title">Health</h3>
                 </a>
                 <p class="card-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
@@ -159,8 +159,7 @@
               </div>
             </div>
           </div>
-        </div>
-        <div class="row">
+
           <div class="col-lg-7">
             <div class="card" data-background="image" style="background-image: url('https://www.goodeed.com/pictures/project/524e6846-5776-4916-9525-dee2d46e385c.JPEG')">
               <div class="btn-group-toggle favourite" data-toggle="buttons">
@@ -169,9 +168,9 @@
                   </label>
               </div>
               <div class="card-body">
-                    <h6 class="card-category">Header 1</h6>
+                    <!-- <h6 class="card-category">Header 1</h6> -->
                 <a href="#">
-                    <h3 class="card-title">Header 2</h3>
+                    <h3 class="card-title">Art and culture</h3>
                 </a>
                 <p class="card-description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
@@ -183,77 +182,10 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-5">
-            <div class="card" data-background="image" style="background-image: url('https://www.goodeed.com/pictures/project/7c511d09-33f4-4ff0-bb3e-95a18d42d4ae.JPEG')">
-              <div class="btn-group-toggle favourite" data-toggle="buttons">
-                  <label class="btn btn-outline-success btn-round">
-                    <input type="checkbox" alt="Submit" autocomplete="off"> <i class="nc-icon nc-favourite-28"></i>
-                  </label>
-              </div>
-              <div class="card-body">
-                    <h6 class="card-category">Header 1</h6>
-                <a href="#">
-                    <h3 class="card-title">Header 2</h3>
-                </a>
-                <p class="card-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                </p>
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-danger btn-round">Donate</button>
-                    <button type="button" class="btn btn-success btn-round">Donate for free</button>                    
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-5">
-            <div class="card" data-background="image" style="background-image: url('https://www.goodeed.com/pictures/project/0b1849e7-721a-463f-ab85-8f3d52ea82c4.JPEG')">
-                <div class="btn-group-toggle favourite" data-toggle="buttons">
-                  <label class="btn btn-outline-success btn-round">
-                    <input type="checkbox" alt="Submit" checked autocomplete="off"> <i class="nc-icon nc-favourite-28"></i>
-                  </label>
-                </div>            
-              <div class="card-body">
-                <h6 class="card-category">Header 1</h6>
-                <a href="#">
-                  <h3 class="card-title">Header 2</h3>
-                </a>
-                <p class="card-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                </p>
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-danger btn-round">Donate</button>
-                    <button type="button" class="btn btn-success btn-round">Donate for free</button>                    
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-7">
-            <div class="card" data-background="image" style="background-image: url('https://www.goodeed.com/pictures/project/dbdf8ee9-ed61-4359-aac5-0a3e927fca2d.JPEG')">
-                <div class="btn-group-toggle favourite" data-toggle="buttons">
-                  <label class="btn btn-outline-success btn-round">
-                    <input type="checkbox" alt="Submit" autocomplete="off"> <i class="nc-icon nc-favourite-28"></i>
-                  </label>
-                </div>
-                <div class="card-body">
-                <h6 class="card-category">Header 1</h6>
-                <a href="#">
-                    <h3 class="card-title">Header 2</h3>
-                </a>
-                <p class="card-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....
-                </p>
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-danger btn-round">Donate</button>
-                    <button type="button" class="btn btn-success btn-round">Donate for free</button>                    
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
   </div>
-  <div class="row mt-2">
+  <div class="mt-2">
     <div class="col-12">
       <div class="card">
         <div class="card-body bg-success d-flex justify-content-center align-items-center rounded">
