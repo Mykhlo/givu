c28f28c54aaef7eba5172398d70b7cc6e7b4f2d4
Mykhailo <mykhailo.tomov@jointoit.com>
2019-07-11 16:00:51 +0300

    customer registration validation -70%

--- resources/views/registration/customer/registration.blade.php
+++ resources/views/registration/customer/registration.blade.php
@@ -14,96 +14,94 @@
         </div>
         <div class="card-body">
             <form id='first_step'>
-            <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-first" role="tabpanel" aria-labelledby="pills-first-tab">
-                    
-                        <div class="form-row mt-3">
-                            <div class="form-group col-md-6">                            
-                                <input type="text" name="first_name" class="form-control" id="inputFirstName" placeholder="First Name" required>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-first" role="tabpanel" aria-labelledby="pills-first-tab">                        
+                            <div class="form-row mt-3">
+                                <div class="form-group col-md-6">                            
+                                    <input type="text" name="first_name" class="form-control" id="inputFirstName" placeholder="First Name" required>
+                                </div>
+                                <div class="form-group col-md-6">                            
+                                    <input type="text" name="last_name" class="form-control" id="inputLastName" placeholder="Last Name" required>
+                                </div>
                             </div>
-                            <div class="form-group col-md-6">                            
-                                <input type="text" name="last_name" class="form-control" id="inputLastName" placeholder="Last Name" required>
+                            <div class="form-row mt-3">
+                                <div class="form-group col-md-6">                            
+                                    <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email" required>
+                                </div>
+                                <div class="form-group col-md-6">                            
+                                    <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password" required>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-row mt-3">
-                            <div class="form-group col-md-6">                            
-                                <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email" required>
+                            <div class="form-row mt-3">
+                                <div class="form-group col-md-6">                                
+                                    <input type="birthday" name="birthday" class="form-control" id="inputbirthday" placeholder="Birthday" required>    
+                                </div>
+                                <div class="form-group col-md-6">                                
+                                    <select id="inputGender" name="gender" class="form-control" required>
+                                        <option selected value="">Gender...</option>
+                                        <option value="male">male</option>
+                                        <option value="female">female</option>
+                                        <option value="other">other</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div class="form-group col-md-6">                            
-                                <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password" required>
-                            </div>
-                        </div>
-                        <div class="form-row mt-3">
-                            <div class="form-group col-md-6">                                
-                                <input type="birthday" name="birthday" class="form-control" id="inputbirthday" placeholder="Birthday" required>    
-                            </div>
-                            <div class="form-group col-md-6">                                
-                                <select id="inputGender" name="gender" class="form-control" required>
-                                    <option selected value="">Gender...</option>
-                                    <option value="male">male</option>
-                                    <option value="female">female</option>
-                                    <option value="other">other</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-row mt-3">
-                            <div class="form-group col-md-6">                                
-                                <select id="inputStatus" name="status" class="form-control" required>
-                                    <option value="" selected> Parental status...</option>
-                                    <option value="with">with children</option>
-                                    <option value="without">without children</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-6">                                
-                                <input type="number" name="income" class="form-control" id="inputIncome" placeholder="Monthly household income" required>  
-                            </div>
-                        </div>                        
-                        <button type="button" class="btn btn-success offset-md-10 col-md-2">Next</button>
-                    
-
-                </div>
-                <div class="tab-pane fade" id="pills-second" role="tabpanel" aria-labelledby="pills-second-tab">
-                <div class="row">
-                    <div class="col-5 col-md-3">   
-                        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                        @foreach($categories as $category)
-                            @if($loop->first)
-                                <a class="nav-link font-weight-bold active" id="v-pills-{{$category->id}}-tab" data-toggle="pill" href="#v-pills-{{$category->id}}" role="tab" aria-controls="v-pills-{{$category->id}}" aria-selected="true">{{$category->name}}</a>
-                            @else
-                                <a class="nav-link font-weight-bold" id="v-pills-{{$category->id}}tab" data-toggle="pill" href="#v-pills-{{$category->id}}" role="tab" aria-controls="v-pills-{{$category->id}}" aria-selected="false">{{$category->name}}</a>
-                            @endif
-                        @endforeach
-                        </div>
+                            <div class="form-row mt-3">
+                                <div class="form-group col-md-6">                                
+                                    <select id="inputStatus" name="status" class="form-control" required>
+                                        <option value="" selected> Parental status...</option>
+                                        <option value="with">with children</option>
+                                        <option value="without">without children</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-6">                                
+                                    <input type="number" name="income" class="form-control" id="inputIncome" placeholder="Monthly household income" required>  
+                                </div>
+                            </div>                        
+                            <button type="button" class="btn btn-success offset-md-10 col-md-2">Next</button>
                     </div>
-                    <div class="col-7 col-md-9 text-secondary">
-                        <div class="tab-content" id="v-pills-tabContent">
-                            @foreach($categories as $category)                                                     
-                                @if($loop->first)                                   
-                                    <div class="tab-pane fade show active" id="v-pills-{{$category->id}}" role="tabpanel" aria-labelledby="v-pills-{{$category->id}}-tab">
-                                @else  
-                                    <div class="tab-pane fade" id="v-pills-{{$category->id}}" role="tabpanel" aria-labelledby="v-pills-{{$category->id}}-tab"> 
-                                @endif   
-                                        <div class="row mt-3 items">
-                                            @foreach($category->subcategories as $subcategory) 
-                                                    @if($loop->count > 1)
-                                                        <h3 {{$loop->first ? "class=mt-0":''}}>{{$subcategory->name}}</h3>
-                                                    @endif
-                                                        @foreach($subcategory->items as $item)   
-                                                        <div class="custom-control custom-checkbox">                                    
-                                                            <input type="checkbox" id="target-{{$item->id}}" class="custom-control-input" value="{{$item->id}}" name="target_checkboxes[]">
-                                                            <label class="custom-control-label pt-1" for="target-{{$item->id}}">{{$item->name}}</label>
-                                                        </div>
-                                                        @endforeach	
-                                            @endforeach                                                                                      
-                                        </div>																															
-                                    </div>
+                    <div class="tab-pane fade" id="pills-second" role="tabpanel" aria-labelledby="pills-second-tab">
+                    <div class="row">
+                        <div class="col-5 col-md-3">   
+                            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            @foreach($categories as $category)
+                                @if($loop->first)
+                                    <a class="nav-link font-weight-bold active" id="v-pills-{{$category->id}}-tab" data-toggle="pill" href="#v-pills-{{$category->id}}" role="tab" aria-controls="v-pills-{{$category->id}}" aria-selected="true">{{$category->name}}</a>
+                                @else
+                                    <a class="nav-link font-weight-bold" id="v-pills-{{$category->id}}tab" data-toggle="pill" href="#v-pills-{{$category->id}}" role="tab" aria-controls="v-pills-{{$category->id}}" aria-selected="false">{{$category->name}}</a>
+                                @endif
                             @endforeach
+                            </div>
                         </div>
+                        <div class="col-7 col-md-9 text-secondary">
+                            <div class="tab-content" id="v-pills-tabContent">
+                                @foreach($categories as $category)                                                     
+                                    @if($loop->first)                                   
+                                        <div class="tab-pane fade show active" id="v-pills-{{$category->id}}" role="tabpanel" aria-labelledby="v-pills-{{$category->id}}-tab">
+                                    @else  
+                                        <div class="tab-pane fade" id="v-pills-{{$category->id}}" role="tabpanel" aria-labelledby="v-pills-{{$category->id}}-tab"> 
+                                    @endif   
+                                            <div class="row mt-3 items">
+                                                @foreach($category->subcategories as $subcategory) 
+                                                        @if($loop->count > 1)
+                                                            <h3 {{$loop->first ? "class=mt-0":''}}>{{$subcategory->name}}</h3>
+                                                        @endif
+                                                            @foreach($subcategory->items as $item)   
+                                                            <div class="custom-control custom-checkbox">                                    
+                                                                <input type="checkbox" id="target-{{$item->id}}" class="custom-control-input" value="{{$item->id}}" name="target_checkboxes[]">
+                                                                <label class="custom-control-label pt-1" for="target-{{$item->id}}">{{$item->name}}</label>
+                                                            </div>
+                                                            @endforeach	
+                                                @endforeach                                                                                      
+                                            </div>																															
+                                        </div>
+                                @endforeach
+                            </div>                            
+                        </div>
+                        <button type="button" class="btn btn-success offset-md-10 col-md-2">Next</button>
                     </div>
-                  </div>
-                </div>
-            </form>
-            </div>            
+                    </div>            
+                </div>  
+            </form>          
         </div>
     </div>
 </div>
